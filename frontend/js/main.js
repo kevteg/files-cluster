6fd86f1d95243f5a9020f27a9b29d7f92d83a861
estefanycss <estefanycss@gmail.com>
2016-09-10 20:48:24 -0400

    front js and css

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,3 +12,34 @@
 	}
 
 }).call(this);
+
+$(".new").click(function(){
+    $("body").removeClass('menu-active')
+		console.log("new");
+		$('#newModal').modal('show');
+		//$('.modal-backdrop').remove();
+});
+
+$(".delete").click(function(){
+    $("body").removeClass('menu-active')
+		console.log("delete");
+		$('#deleteModal').modal('show');
+});
+
+$(".sync").click(function(){
+    $("body").removeClass('menu-active')
+		console.log("sync");
+
+});
+
+$(".settings").click(function(){
+    $("body").removeClass('menu-active')
+		console.log("settings");
+		$(".main-panel").css("display","none")
+		$(".setting-panel").css("display","block")
+});
+
+$("#close-setting").click(function(){
+	$(".main-panel").css("display","block")
+	$(".setting-panel").css("display","none")
+});
